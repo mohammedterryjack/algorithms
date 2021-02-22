ef0969539cff500dec75014c4e1673e41861dadd
mohammedterryjack <67234383+mohammedterryjack@users.noreply.github.com>
2021-02-22 00:16:35 +0000

    Update sorting.py

--- sorting.py
+++ sorting.py
@@ -1,15 +1,15 @@
 from typing import List
 
 def quick_sort(values:List[int]) -> List[int]:
-    if len(values) == 0: return values
+    if not any(values): return values
 
     pivot = values.pop()
     less,equal,greater = [],[pivot],[]
     for value in values:
         if value < pivot:
             less.append(value)
-        elif value == pivot:
-            equal.append(value)
-        else:
+        elif value > pivot:
             greater.append(value)
+        else:
+            equal.append(value)
     return quick_sort(less)+equal+quick_sort(greater) 
