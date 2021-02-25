87a1b6b107787f0cdea1356eb194d54e414bf6ab
mohammedterryjack <67234383+mohammedterryjack@users.noreply.github.com>
2021-02-25 00:02:21 +0000

    Update searching.py

--- searching.py
+++ searching.py
@@ -16,10 +16,16 @@ def index(values:List[int], target_value:int) -> int:
 
 def binary_search(ordered_indexed_values:List[Tuple[int,int]], target_value:int) -> int:
     if not any(ordered_indexed_values): return -1
+    
     middle_index = len(ordered_indexed_values)//2
     index,value = ordered_indexed_values[middle_index]
+    
     if target_value==value: 
         return index
-    if target_value < value: 
-        return binary_search(ordered_indexed_values[:middle_index],target_value)
-    return binary_search(ordered_indexed_values[middle_index+1:],target_value)
+    return binary_search(
+        ordered_index_values=ordered_indexed_values[:middle_index],
+        target_value=target_value
+    ) if target_value < value else binary_search(
+        ordered_index_values=ordered_indexed_values[middle_index+1:],
+        target_value=target_value
+    )
