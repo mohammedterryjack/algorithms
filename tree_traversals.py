3108846f6d7c4fc5e9d4c537939ddc8bf45be48a
mohammedterryjack <67234383+mohammedterryjack@users.noreply.github.com>
2021-02-25 10:34:53 +0000

    Create tree_traversals.py

---
+++ tree_traversals.py
@@ -0,0 +1,40 @@
+from typing import List,Dict,Set
+
+def breadth_first_traversal(
+    graph:Dict[str,List[str]], 
+    nodes_queue:List[str], 
+    seen:Set[str]
+) -> None:
+    if not any(nodes_queue): return 
+    node = nodes_queue.pop(0)
+    if node not in seen: 
+        print(node)
+        connected_nodes = graph.get(node)
+        nodes_queue += connected_nodes
+        seen.add(node)
+    breadth_first_traversal(graph,nodes_queue,seen)
+
+def depth_first_traversal(
+    graph:Dict[str,List[str]], 
+    nodes_queue:List[str], 
+    seen:Set[str]
+) -> None:
+    if not any(nodes_queue): return
+    node = nodes_queue.pop()
+    if node not in seen:
+        print(node)
+        connected_nodes = graph.get(node)
+        nodes_queue += connected_nodes
+        seen.add(node)
+    depth_first_traversal(graph,nodes_queue,seen)
+    
+#example_graph = {
+#  'A' : ['B','C'],
+#  'B' : ['D', 'E'],
+#  'C' : ['F'],
+#  'D' : ['D'],
+#  'E' : ['F'],
+#  'F' : []
+#}
+#breadth_first_traversal(example_graph,["A"],set())
+#depth_first_traversal(example_graph,["A"],set())
