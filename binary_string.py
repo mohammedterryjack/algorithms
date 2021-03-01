7f652717251dc5a609a4a3f58b084a69c0e7f636
mohammedterryjack <67234383+mohammedterryjack@users.noreply.github.com>
2021-03-01 13:07:29 +0000

    Update binary_string.py

--- binary_string.py
+++ binary_string.py
@@ -3,16 +3,22 @@ from typing import Union
 class BinaryString:
     def __init__(self, value:Union[str,int], length:int=10) -> None:
         if isinstance(value,str):
-            self.binary_string = value
-            self.length = len(value)
-            self.integer_value = self.convert_binary_string_to_int(self.binary_string)
+            self.initialise_binary_string(value)
         elif isinstance(value,int):
-            self.length = length
-            self.integer_value = value
-            self.binary_string = self.convert_integer_to_binary_string(self.integer_value,self.length)
+            self.initialise_integer_value(value,length)
         else:
             raise ValueError
     
+    def initialise_binary_string(self,binary_string:str) -> None:
+        self.integer_value = self.convert_binary_string_to_int(binary_string)
+        self.binary_string = binary_string
+        self.length = len(binary_string)
+
+    def initialise_integer_value(self,integer_value:int,length:int) -> None:
+        self.integer_value = integer_value
+        self.binary_string = self.convert_integer_to_binary_string(integer_value,length)
+        self.length = len(self.binary_string)
+
     def __str__(self) -> str:
         return self.binary_string
     
@@ -23,10 +29,8 @@ class BinaryString:
         return self
 
     def __next__(self) -> str:
-        self.integer_value += 1
-        self.binary_string = self.convert_integer_to_binary_string(self.integer_value,self.length)
         old_length = self.length
-        self.length = len(self.binary_string)
+        self.initialise_integer_value(self.integer_value+1, self.length)
         if self.length > old_length:
             raise StopIteration
         return self 
@@ -39,7 +43,6 @@ class BinaryString:
     def convert_binary_string_to_int(binary_string:str) -> int:
         return int(binary_string,base=2)
 
-
-#efficiently explore combinations
-#for permutation in BinaryString("00000"):
-#    print(permutation)
+# for permutation in BinaryString("000"):
+#     chosen_indices = list(map(int,str(permutation)))
+#     print(permutation, chosen_indices)
