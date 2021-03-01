8a68c3696f158939ea1cc830bfa047faf99be2f7
mohammedterryjack <67234383+mohammedterryjack@users.noreply.github.com>
2021-03-01 13:29:39 +0000

    Update and rename binary_string.py to bit_vector.py

--- bit_vector.py
+++ bit_vector.py
@@ -1,6 +1,6 @@
-from typing import Union
+from typing import Union, List
 
-class BinaryString:
+class BitVector:
     def __init__(self, value:Union[str,int], length:int=10) -> None:
         if isinstance(value,str):
             self.initialise_binary_string(value)
@@ -12,26 +12,30 @@ class BinaryString:
     def initialise_binary_string(self,binary_string:str) -> None:
         self.integer_value = self.convert_binary_string_to_int(binary_string)
         self.binary_string = binary_string
-        self.length = len(binary_string)
 
     def initialise_integer_value(self,integer_value:int,length:int) -> None:
         self.integer_value = integer_value
         self.binary_string = self.convert_integer_to_binary_string(integer_value,length)
-        self.length = len(self.binary_string)
+
+    def as_array(self) -> List[int]:
+        return list(map(int,self.binary_string))
 
     def __str__(self) -> str:
         return self.binary_string
     
     def __int__(self) -> int:
         return self.integer_value
-    
+        
+    def __len__(self) -> int:
+        return len(self.binary_string)
+
     def __iter__(self):
         return self
 
     def __next__(self) -> str:
-        old_length = self.length
-        self.initialise_integer_value(self.integer_value+1, self.length)
-        if self.length > old_length:
+        old_length = len(self)
+        self.initialise_integer_value(self.integer_value+1, old_length)
+        if len(self) > old_length:
             raise StopIteration
         return self 
 
@@ -43,6 +47,5 @@ class BinaryString:
     def convert_binary_string_to_int(binary_string:str) -> int:
         return int(binary_string,base=2)
 
-# for permutation in BinaryString("000"):
-#     chosen_indices = list(map(int,str(permutation)))
-#     print(permutation, chosen_indices)
+# for permutation in BitVector("000"):
+#     print(permutation, permutation.as_array())
