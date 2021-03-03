6c5b0843a169b861dfaa343425e1f66816a8cb9d
mohammedterryjack <67234383+mohammedterryjack@users.noreply.github.com>
2021-03-03 23:38:36 +0000

    Update recursive_power.py

--- recursive_power.py
+++ recursive_power.py
@@ -1,6 +1,7 @@
+def is_odd(number:int) -> bool:
+    return bool(number%2)
+
 def power_efficiently(number:float, power:int) -> float:
-    def is_odd(number:int) -> bool:
-        return bool(number%2)
     if not(power): return 1.
     result = power_efficiently(number,power//2)**2
     if is_odd(power): result *= number  
