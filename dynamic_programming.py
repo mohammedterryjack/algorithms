d56b6c280551611c170832632254c81753663072
mohammedterryjack <67234383+mohammedterryjack@users.noreply.github.com>
2021-03-05 15:15:38 +0000

    Update dynamic_programming.py

--- dynamic_programming.py
+++ dynamic_programming.py
@@ -4,3 +4,13 @@ def fibonacci(n:int, memory:dict={}) -> int:
     return memory[n]
 
 #print(fibonacci(50))
+
+
+def grid_traveller(x:int,y:int,memory:dict={}) -> int:
+    if x==0 or y==0: return 0
+    if x==1 and y==1: return 1
+    coordinates = ','.join(map(str,sorted((x,y))))
+    if coordinates not in memory: memory[coordinates] = grid_traveller(x-1,y) + grid_traveller(x,y-1)
+    return memory[coordinates]
+
+#print(grid_traveller(18,18))
