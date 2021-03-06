869ddbc0cec382f48cf9101f16242aea9ffdf2f0
mohammedterryjack <67234383+mohammedterryjack@users.noreply.github.com>
2021-03-06 21:21:07 +0000

    Update iteration_and_tabulation.py

--- iteration_and_tabulation.py
+++ iteration_and_tabulation.py
@@ -1,4 +1,4 @@
-
+from typing import List
 
 def fibonacci(n:int) -> int:
     memory = [0]*(n+2)
@@ -9,3 +9,18 @@ def fibonacci(n:int) -> int:
     return memory[n]
 
 #print(50,fibonacci(50))
+
+
+def create_sum_with_least_values(target_sum:int, values:List[int]) -> List[int]:
+    memory = [None]*(target_sum + max(values))
+    memory[0] = []
+    for index in range(target_sum):
+        if memory[index] is not None:
+            subvalues = memory[index]
+            for offset in values:
+                new_subvalues = subvalues + [offset]
+                if memory[index+offset] is None or len(new_subvalues) < len(memory[index+offset]): 
+                    memory[index+offset] = new_subvalues
+    return memory[target_sum]
+
+#print(create_sum_with_least_values(111,[3,23,10]))
