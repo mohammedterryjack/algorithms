aab1f4b22fdc9d7f0c3341907981a8151a9a789f
mohammedterryjack <67234383+mohammedterryjack@users.noreply.github.com>
2021-03-09 18:10:08 +0000

    Update traversing_trees.py

--- traversing_trees.py
+++ traversing_trees.py
@@ -8,35 +8,35 @@ class Node:
         return self.name
 
 
-def depth_first_search(stack:List[Node]) -> None:
-    if not any(stack): return 
+def depth_first_search(stack:List[Node]) -> int:
+    if not any(stack): return 0 
     node = stack.pop()
     print(node)
     stack.extend(node.children)
-    return depth_first_search(stack)
+    return node.value + depth_first_search(stack)
 
-def breadth_first_search(queue:List[Node]) -> None:
-    if not any(queue): return 
+def breadth_first_search(queue:List[Node]) -> int:
+    if not any(queue): return 0
     node = queue.pop(0)
     print(node)
     queue.extend(node.children)
-    return breadth_first_search(queue)
+    return node.value + breadth_first_search(queue)
 
-def recursive_sum(node:Node) -> int:
+def simplified_breadth_first_search(node:Node) -> int:
     if node is None: return 0
-    print(node)
-    return node.value + sum(map(recursive_sum,node.children))
+    return node.value + sum(map(simplified_breadth_first_search,node.children))
 
-#a = Node("a",1)
-#b = Node("b",2)
-#c = Node("c",3)
-#d = Node("d",4)
-#e = Node("e",5)
-#f = Node("f",6)
-#g = Node("g",7)
-#b.children = [d,e]
-#c.children = [f,g]
-#a.children = [b,c]
-#print(recursive_sum(a))
-#print(breadth_first_search([a]))
-#print(depth_first_search([a]))
+# a = Node("a",1)
+# b = Node("b",2)
+# c = Node("c",3)
+# d = Node("d",4)
+# e = Node("e",5)
+# f = Node("f",6)
+# g = Node("g",7)
+# b.children = [d,e]
+# c.children = [f,g]
+# a.children = [b,c]
+# print(depth_first_search([a]))
+# print(breadth_first_search([a]))
+# print(simplified_breadth_first_search(a))
+
